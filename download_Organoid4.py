24c7a3ca8cadb69c8411ac1c750012931b6643ed
Luke Zappia <lazappi@users.noreply.github.com>
2023-09-12 21:45:24 +0200

    Add download_Organoid4.py

--- download_Organoid4.py
+++ download_Organoid4.py
@@ -11,19 +11,19 @@ import shutil
 
 lb.settings.species = "human"
 
-transform = ln.Transform(name="download_Organoid123")
+transform = ln.Transform(name="download_Organoid4")
 ln.track(transform)
 
 base_url = "https://ftp.ncbi.nlm.nih.gov/geo/series/GSE114nnn/GSE114802/suppl/"
 ssl_context = ssl.create_default_context(cafile=certifi.where())
 
-data_dir = "data/00-raw/Organoid123"
+data_dir = "data/00-raw/Organoid4"
 print(f"Downloading dataset from '{base_url}'...")
 if not os.path.exists(data_dir):
    os.makedirs(data_dir)
 
 print(f"Downloading genes...")
-genes_url = base_url + "GSE114802_org_genes.tsv.gz"
+genes_url = base_url + "GSE114802_org4_genes.tsv.gz"
 genes_path = os.path.join(data_dir, "genes.tsv")
 with urlopen(genes_url, context=ssl_context) as url_file:
     with open(genes_path + ".gz", "wb") as out_file:
@@ -35,7 +35,7 @@ with urlopen(genes_url, context=ssl_context) as url_file:
 genes_file = ln.File(genes_path, key=genes_path)
 
 print(f"Downloading barcodes...")
-barcodes_url = base_url + "GSE114802_org_barcodes.tsv.gz"
+barcodes_url = base_url + "GSE114802_org4_barcodes.tsv.gz"
 barcodes_path = os.path.join(data_dir, "barcodes.tsv")
 with urlopen(barcodes_url, context=ssl_context) as url_file:
     with open(barcodes_path + ".gz", "wb") as out_file:
@@ -47,7 +47,7 @@ with urlopen(barcodes_url, context=ssl_context) as url_file:
 barcodes_file = ln.File(barcodes_path, key=barcodes_path)
 
 print(f"Downloading matrix...")
-matrix_url = base_url + "GSE114802_org_matrix.mtx.gz"
+matrix_url = base_url + "GSE114802_org4_matrix.mtx.gz"
 matrix_path = os.path.join(data_dir, "matrix.mtx")
 with urlopen(matrix_url, context=ssl_context) as url_file:
     with open(matrix_path + ".gz", "wb") as out_file:
@@ -65,7 +65,7 @@ print(adata)
 adata_file = ln.File.from_anndata(
     adata,
     field=lb.Gene.ensembl_gene_id,
-    key="Organoid123_raw"
+    key="Organoid4_raw"
 )
 
 print(f"Saving AnnData object...")
